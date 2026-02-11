fcc46fc05c80533d18af0e8003f98c3403a72e4f
bibibig <dltmdwls154@gmail.com>
2026-02-12 00:00:51 +0900

    Nuxt TailwindCss Setting

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,11 @@
-// https://nuxt.com/docs/api/configuration/nuxt-config
+import tailwindcss from '@tailwindcss/vite'
+import { defineNuxtConfig } from 'nuxt/config'
+
 export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
-  devtools: { enabled: true }
+  devtools: { enabled: true },
+  css: ['./app/assets/styles/index.css'],
+  vite: {
+    plugins: [tailwindcss()],
+  },
 })
